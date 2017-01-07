1dbd708a92561d33098ff15504f3f6e8fd476323
bljubisic <bratislav.ljubisic@gmail.com>
2017-01-07 15:12:45 +0100

    ViewModel for ProfileSelection View Controller

---
+++ LearnAlphabet/SelectProfileViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  SelectProfileViewModel.swift
+//  LearnAlphabet
+//
+//  Created by Bratislav Ljubisic on 1/6/17.
+//  Copyright © 2017 Bratislav Ljubisic. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct SelectProfileViewModel: UITableViewDelegate, UITableViewDataSource  {
+    
+}
