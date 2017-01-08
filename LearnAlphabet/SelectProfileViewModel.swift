037451841a5c698d6c19e5c282ffb5d409ffd23c
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-01-08 09:36:54 +0100

    Added stub methods for ViewModel

--- LearnAlphabet/SelectProfileViewModel.swift
+++ LearnAlphabet/SelectProfileViewModel.swift
@@ -9,8 +9,41 @@
 import Foundation
 import UIKit
 
+struct ProfileStruct {
+    let name: String
+    let surname: String
+    let avatar: String
+    
+}
 
-struct SelectProfileViewModel {
+protocol ProfileViewModelProtocol {
+    func getAllProfiles() -> [ProfileStruct]
+    func getNumberOfAvailableProfiles() -> Int
+    func addNewProfile(newProfile: ProfileStruct) -> Void
+    func selectProfile(selectedProfile: ProfileStruct) -> ProfileStruct
+    
+}
+
+
+struct SelectProfileViewModel: ProfileViewModelProtocol {
+    
+    internal func selectProfile(selectedProfile: ProfileStruct) -> ProfileStruct {
+        
+    }
+
+    internal func addNewProfile(newProfile: ProfileStruct) {
+        
+    }
+
+    
+    internal func getNumberOfAvailableProfiles() -> Int {
+        
+    }
+
+    internal func getAllProfiles() -> [ProfileStruct] {
+        
+    }
+
     
     
     
