379fa006c22e58953cb32138e7730a0875110fc5
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-01-25 15:40:51 +0100

    test for json processing model

--- LearnAlphabetTests/LearnAlphabetTests.swift
+++ LearnAlphabetTests/LearnAlphabetTests.swift
@@ -21,16 +21,24 @@ class LearnAlphabetTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testReadAlphabets() {
+        let alphabetJSONReader: LearnAlphabetJSONReader = LearnAlphabetJSONReader()
+        
+        let arrayOfAlphabets:[AlphabetStruct]? = alphabetJSONReader.readAlphabets()
+        
+        assert(arrayOfAlphabets != nil, "Array is nil")
+        
+        
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testReadAvatars() {
+        let alphabetJSONReader: LearnAlphabetJSONReader = LearnAlphabetJSONReader()
+        
+        let arrayOfAvatars:[AvatarStruct]? = alphabetJSONReader.readJSONAvatars()
+        
+        assert(arrayOfAvatars != nil, "Array is nil")
+        
+        
     }
     
 }
