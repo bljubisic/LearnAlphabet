745e5b76abca774cdb95bee59c5e83a295cf0f5a
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-01-28 22:19:25 +0100

    NSManagedObject autogenerated

---
+++ LearnAlphabet/Profile+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Profile+CoreDataClass.swift
+//  LearnAlphabet
+//
+//  Created by Bratislav Ljubisic on 1/28/17.
+//  Copyright © 2017 Bratislav Ljubisic. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Profile: NSManagedObject {
+
+}
