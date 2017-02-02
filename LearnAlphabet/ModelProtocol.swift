16857c1da21b1f1e9b68abdd022957bfb1dc73d9
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-02-02 16:08:08 +0100

    Changes in Profile and ProfileProgress structs

--- LearnAlphabet/ModelProtocol.swift
+++ LearnAlphabet/ModelProtocol.swift
@@ -9,16 +9,38 @@
 import Foundation
 
 
-struct ProfileStruct {
+public struct ProfileStruct {
     let firstname: String
     let lastname: String
+    let language: LanguageEnum
+    let yearsold: Int
     let avatar: AvatarStruct
     let alphabet: AlphabetStruct
 }
 
-struct ProfileProgressStruct {
+extension ProfileStruct {
+    init() {
+        firstname = ""
+        lastname = ""
+        language = LanguageEnum(rawValue: "")!
+        yearsold = 0
+        avatar = AvatarStruct()
+        alphabet = AlphabetStruct()
+    }
+}
+
+public struct ProfileProgressStruct {
+    let firstname: String
     let alphabet: AlphabetStruct
     let character: String
+}
+
+extension ProfileProgressStruct {
+    init() {
+        firstname = ""
+        alphabet = AlphabetStruct()
+        character = ""
+    }
 }
 
 struct AvatarStruct {
@@ -26,10 +48,35 @@ struct AvatarStruct {
     let image: String
 }
 
+extension AvatarStruct {
+    init() {
+        name = ""
+        image = ""
+    }
+}
+
 struct AlphabetStruct {
     let name: String
     let characters: [String]
     let numofCharacters: Int
+}
+
+extension AlphabetStruct {
+    init() {
+        name = ""
+        characters = [""]
+        numofCharacters = 0
+    }
+}
+enum LanguageEnum: String {
+    case English
+    case Serbian
+    case Chinese
+}
+
+enum Keys: String {
+    case Firstname = "firstname"
+    case Alphabetname = "alphabetname"
 }
 
 protocol EnvironmentProtocol {
@@ -46,6 +93,7 @@ protocol LearnAlphabetCoreDataProtocol {
     var environment: EnvironmentProtocol? { get }
     
     func set(Environment: EnvironmentProtocol) -> Void
+    
     func getProfiles() -> [ProfileStruct]
     func insert(profile newProfile:ProfileStruct)
     func getProfileWith(name profileName: String) -> ProfileStruct
