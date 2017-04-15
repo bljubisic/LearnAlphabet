056f4814d6f61b52a45e6639f3ee2eb10bbf3c57
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-04-15 21:21:16 +0200

    Added stub for AddProfileViewController

---
+++ LearnAlphabet/AddProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddProfileViewController.swift
+//  LearnAlphabet
+//
+//  Created by Bratislav Ljubisic on 3/8/17.
+//  Copyright © 2017 Bratislav Ljubisic. All rights reserved.
+//
+
+import UIKit
+
+class AddProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
