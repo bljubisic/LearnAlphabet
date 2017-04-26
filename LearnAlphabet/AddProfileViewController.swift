0bb8d3ac83f97a15b6e1eeeab9de726c4b4967ae
Bratislav Ljubisic <Bratislav.Ljubisic@plantronics.com>
2017-04-26 17:43:22 +0200

    Added stub fields

--- LearnAlphabet/AddProfileViewController.swift
+++ LearnAlphabet/AddProfileViewController.swift
@@ -7,11 +7,35 @@
 //
 
 import UIKit
+import SnapKit
 
 class AddProfileViewController: UIViewController {
 
+    var avatarButton: UIButton!
+    var firstNameText: UITextField!
+    var lastNameText: UITextField!
+    var yearBornLabel: UILabel!
+    var yearBornPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.avatarButton = UIButton()
+        self.firstNameText = UITextField()
+        self.lastNameText = UITextField()
+        self.yearBornLabel = UILabel()
+        self.yearBornPicker = UIPickerView()
+        
+        self.view.addSubview(avatarButton)
+        self.view.addSubview(firstNameText)
+        self.view.addSubview(lastNameText)
+        self.view.addSubview(yearBornLabel)
+        self.view.addSubview(yearBornPicker)
+        
+        self.avatarButton.snp.makeConstraints { (make) in
+            make.edges.centerX.equalTo(self.view)
+        }
+        
+        
     }
 
 }
